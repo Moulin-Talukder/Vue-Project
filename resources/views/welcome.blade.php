912c640f66b99254696b323200be4ecd409a6221
Moulin Talukder <65370778+Moulin-Talukder@users.noreply.github.com>
2022-01-06 03:17:20 +0600

    2nd commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>laravel</title>
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="url" content="{{ url('') }}">
